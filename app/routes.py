4eb53e2783f1570d592b79e2dfe51f19439b38da
Morgan <morganheuer@gmail.com>
2022-04-26 10:55:56 -0700

    Wave 2 complete

--- app/routes.py
+++ app/routes.py
@@ -29,3 +29,22 @@ def get_all_planets():
             }
         ) 
     return jsonify(response), 200
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_planet(planet_id):
+    return_planet = None
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        return jsonify({'msg': f"Invalid planet ID: '{planet_id}'. Planet ID must be an integer."}), 400
+    for planet in planets:
+        if planet.id == planet_id:
+            return_planet = {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "moons": planet.moons
+            }
+    if return_planet is None:
+        return jsonify({'msg': f"no planet with id '{planet_id}' found"}), 404
+    return jsonify(return_planet)
